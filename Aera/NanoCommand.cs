c3de17d158871ba8bbb088bdc407205f4a539854
SimeonDijksma <1472585@student.rocvantwente.nl>
2026-02-12 11:12:20 +0100

    setup nano command

--- Aera/NanoCommand.cs
+++ Aera/NanoCommand.cs
@@ -1,6 +1,32 @@
-namespace Aera;
-
-public class NanoCommand
+namespace Aera
 {
+    // hypothetical
     
+    // save current console to console.txt,
+    // run second program Nano,
+    // Nano runs and does what it should,
+    // Nano closes starts aera with a variable that skips login and loads console.txt,
+    // console.txt loads and makes console content reappear as if nothing happened.
+    
+    internal class NanoCommand : ICommand
+    {
+        public string Name => "nano <file>";
+        public string Description => "Edit document contents";
+        public string Usage => "Usage: nano";
+
+        public bool AcceptsPipeInput => false;
+        public bool IsDestructive => true;
+
+        public string[] Aliases => Array.Empty<string>();
+
+        public void Execute(string[] args, ShellContext tool)
+        {
+            tool.WriteLineColored("This command is under construction", "yellow");
+        }
+
+        public void ExecutePipe(string input, string[] args, ShellContext tool)
+        {
+            tool.WriteLineColored("nano: cannot be used in a pipe", "Red");
+        }
+    }
 }
